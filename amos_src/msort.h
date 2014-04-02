756fa409bad44465737f224698419966e2395a3f
Amos Byon <abyon@usc.edu>
2014-04-02 07:19:09 -0700

    added msort.h that contains impl of merge sort

---
+++ amos_src/msort.h
@@ -0,0 +1,47 @@
+#ifndef MSORT_H
+#define MSORT_H
+
+#include <cstdlib>
+#include <deque>
+
+using namespace std;
+
+template <class T, class Comparator>
+void mergeSort (deque<T> myArray, Comparator comp){
+	if(1 < myArray.size()){
+		deque<T> arr1(myArray.begin(), myArray.begin()+array.size()/2);
+		mergeSort(arr1, comp);
+		deque<T> arr2(myArray.begin()+array.size()/2, myArray.end());
+		mergeSort(arr2, comp);
+
+		myArray = merge(arr1, arr2, comp);
+	}
+}
+
+template <class T, class Comparator>
+deque<T> merge(deque<T> arr1, deque<T> arr2, Comparator comp){
+	deque<T> result;
+	while(!arr1.empty() && !arr2.empty()){
+		if(comp(arr1.front(), arr2.front())){
+			result.push_back(arr1.front());
+			arr1.pop_front();
+		}else{
+			result.push_back(arr2.front());
+			arr2.pop_front();
+		}
+	}
+	if(arr1.empty() && !arr2.empty()){
+		while(!arr2.empty()){
+			result.push_back(arr2.front());
+			arr2.pop_front();
+		}
+	}else if(!arr1.empty() && arr2.empty()){
+		while(!arr1.empty()){
+			result.push_back(arr1.front());
+			arr1.pop_front();
+		}
+	}
+	return result;
+}
+
+#endif
